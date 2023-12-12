e7e743d940f798beaee6224590dae0da011a2c01
Nikita Osovskiy <nikitaosovskiy2005ok181@gmail.com>
2023-12-12 20:31:55 +0300

    done: hw lab5

--- lab1/main.py
+++ lab1/main.py
@@ -6,17 +6,25 @@ import numpy as np
 import tkinter as tk
 from tkinter import filedialog as fd
 
+
 def process_image_chunk(chunk, output_folder):
     image_path, chunk_index, chunk_data = chunk
 
     img = cv2.imread(image_path)
 
-    result_chunk = find_stars_in_chunk(img, chunk_data)
+    result_chunk, contours = find_stars_in_chunk(img, chunk_data)
 
     output_path = os.path.join(output_folder, f"output_{os.path.basename(image_path)}_{chunk_index}.png")
     cv2.imwrite(output_path, result_chunk)
 
-    return {"chunk_index": chunk_index, "output_path": output_path}
+    # Сохранение местоположения объектов
+    obj_locations = []
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        obj_locations.append((x + chunk_data[0], y + chunk_data[1], w, h))
+
+    return {"chunk_index": chunk_index, "output_path": output_path, "obj_locations": obj_locations}
+
 
 
 def find_stars_in_chunk(image, chunk_data):
@@ -39,7 +47,7 @@ def find_stars_in_chunk(image, chunk_data):
     for contour in contours:
         cv2.drawContours(chunk, [contour], 0, (0, 255, 0), 2)
 
-    return chunk
+    return chunk, contours
 
 
 def divide_image(image_path, num_chunks):
@@ -62,6 +70,7 @@ def parallel_process_images(image_paths, output_folder,
         results = pool.starmap(process_image_chunk, [(chunk, output_folder) for image_path in image_paths
                                                       for chunk in divide_image(image_path, num_chunks_per_image)])
 
+    obj_locations = []
     img_results = []
     for result in results:
         print(f"Chunk: {result['chunk_index']}, Output: {result['output_path']}")
@@ -70,6 +79,9 @@ def parallel_process_images(image_paths, output_folder,
             img_results.append(img)
         else:
             print(f"Skipping {result['output_path']} due to different size or data type.")
+        obj_locations += [(os.path.basename(result['output_path']), loc) for loc in result['obj_locations']]
+    
+
     result = cv2.hconcat(img_results)
 
     output_path = os.path.join(output_folder, 'result.jpg')
@@ -79,7 +91,17 @@ def parallel_process_images(image_paths, output_folder,
     cv2.imshow('Result', result)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    return results
+
+    # Сохранение местоположения объектов в файл
+    n_elements = 0
+    with open(os.path.join(output_folder, 'obj_locations.txt'), 'w') as f:
+        for image_path, loc in obj_locations:
+            f.write(f"{os.path.basename(image_path)}: x={loc[0]}, y={loc[1]}, w={loc[2]}, h={loc[3]}\n")
+            n_elements += 1
+    print(f"Object locations saved to {os.path.join(output_folder, 'obj_locations.txt')}")
+
+    return results, n_elements
+
 
 
 def find_btn():
@@ -93,7 +115,9 @@ def find_btn():
 
     images = fd.askopenfilenames(filetypes=[('jpg', '*.jpg')])
 
-    parallel_process_images(images, output_folder, num_processes, chunks)
+    results, n_elements = parallel_process_images(images, output_folder, num_processes, chunks)
+    n_elements_label.config(text=f"Found {n_elements} objects")
+
 
 
 if __name__ == "__main__":
@@ -105,6 +129,9 @@ if __name__ == "__main__":
     chunks_label.grid(row=1, column=1)
     chunks_entry = tk.Entry(root)
     chunks_entry.grid(row=1, column=2)
+
+    n_elements_label = tk.Label(root, text="")
+    n_elements_label.grid(row=4, column=1)
 
     btn1 = tk.Button(root, text="find", command=find_btn)
     btn1.grid(row=3, column=1)
---
+++ lab5/main.py
@@ -0,0 +1,154 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, F, Router
+from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup
+from aiogram.filters import Command, CommandStart
+
+import python_weather
+import translators as ts
+from forex_python.converter import CurrencyRates
+
+
+# инициализация бота и хранилища состояний
+
+TOKEN = '6806009909:AAHiwG96UKgt6NcFSx5IJClCtTtlEfzovWw'
+
+form_router = Router()
+
+# настройка логирования
+logging.basicConfig(level=logging.INFO)
+
+
+# определение состояний беседы
+class Conversation(StatesGroup):
+    name = State()
+    age = State()
+    weather = State()
+    currency = State()
+    translate = State()
+
+
+# обработка команды /start
+@form_router.message(CommandStart())
+async def start_command(message: Message, state: FSMContext):
+    await state.set_state(Conversation.name)
+    await message.answer("Привет! Как тебя зовут?")
+
+
+# обработка текстовых сообщений в состоянии waiting_for_name
+@form_router.message(Conversation.name)
+async def process_name(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await state.set_state(Conversation.age)
+    await message.answer("Сколько тебе лет?")
+
+
+# обработка текстовых сообщений в состоянии waiting_for_age
+@form_router.message(Conversation.age)
+async def process_age_invalid(message: Message, state: FSMContext):
+    kb = [
+            [KeyboardButton(text='погода')],
+            [KeyboardButton(text='валюта')],
+            [KeyboardButton(text='переведи')]
+            ]
+    keyboard = ReplyKeyboardMarkup(keyboard=kb)
+    if not message.text.isdigit():
+        await message.answer("Возраст должен быть числом. Сколько тебе лет?")
+    else:
+        data = await state.update_data(age=message.text)
+        data['age'] = message.text
+        name = data['name']
+        age = data['age']
+        await state.clear()
+        await message.answer(f"Приятно познакомиться, {name}! Тебе {age} лет.", 
+                             reply_markup=keyboard)
+
+
+# обработка текстовых сообщений
+@form_router.message(F.text.lower() == 'привет')
+async def greet(message: Message):
+    await message.answer("Привет!")
+
+
+@form_router.message(F.text.lower() == 'погода')
+async def get_weather_message(message: Message, state: FSMContext):
+    await state.set_state(Conversation.weather)
+    await message.answer("В каком городе хотите узнать?")
+
+
+@form_router.message(F.text.lower() == 'валюта')
+async def get_rate_message(message: Message, state: FSMContext):
+    await state.set_state(Conversation.currency)
+    ans = '''|EUR - Euro Member Countries |IDR - Indonesia Rupiah |BGN - Bulgaria Lev |ILS - Israel Shekel |GBP - United Kingdom Pound |DKK - Denmark Krone |CAD - Canada Dollar |JPY - Japan Yen |HUF - Hungary Forint |RON - Romania New Leu |MYR - Malaysia Ringgit |SEK - Sweden Krona |SGD - Singapore Dollar |HKD - Hong Kong Dollar |AUD - Australia Dollar |CHF - Switzerland Franc |KRW - Korea (South) Won |CNY - China Yuan Renminbi |TRY - Turkey Lira |HRK - Croatia Kuna |NZD - New Zealand Dollar |THB - Thailand Baht |USD - United States Dollar |NOK - Norway Krone |INR - India Rupee |MXN - Mexico Peso |CZK - Czech Republic Koruna |BRL - Brazil Real |PLN - Poland Zloty |PHP - Philippines Peso |ZAR - South Africa Rand
+Введите валюты через пробел и сумму (USD EUR 10)?'''
+    await message.answer(ans)
+
+@form_router.message(F.text.lower() == 'переведи')
+async def get_translation_message(message: Message, state: FSMContext):
+    await state.set_state(Conversation.translate)
+    await message.answer("Что перевести?")
+
+
+
+@form_router.message(Conversation.weather)
+async def answer_weather_message(message: Message, state: FSMContext):
+    await state.clear()
+    weather = await get_weather(message.text)
+    await message.answer(weather)
+
+
+@form_router.message(Conversation.currency)
+async def answer_rate_message(message: Message, state: FSMContext):
+    await state.clear()
+    cur1, cur2, amount = message.text.split(' ')
+    rate = await get_rate(cur1, cur2, int(amount))
+    await message.answer(rate)
+
+
+@form_router.message(Conversation.translate)
+async def answer_translation_message(message: Message, state: FSMContext):
+    await state.clear()
+    text = await get_translation(message.text)
+    await message.answer(text)
+
+
+async def get_weather(city: str):
+  async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
+    weather = await client.get(city)
+    text = f'''Температура: {weather.current.temperature} градусов C
+Чувствуется температура как: {weather.current.feels_like} градусов C
+Давление: {weather.current.pressure}
+Осадки: {weather.current.precipitation} мм
+Влажность: {weather.current.humidity} %
+Скорость ветра: {weather.current.wind_speed} км/ч
+'''
+    return text
+
+
+async def get_rate(cur1: str, cur2: str, amount: int):
+    c = CurrencyRates()
+    return f'{c.convert(cur1, cur2, amount)}'
+
+async def get_translation(text: str):
+    return ts.translate_text(text, translator='google', to_language='ru')
+
+
+# обработка неизвестных команд
+@form_router.message()
+async def unknown_command(message: Message):
+    await message.answer("Введите команду.")
+
+
+async def main():
+    bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
+    dp = Dispatcher()
+    dp.include_router(form_router)
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
